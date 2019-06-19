7779c089bc74e2f5f1c95e4cb677a79cbe43231f
djzenma <eidma@aucegypt.edu>
2019-06-19 22:29:32 +0200

    Added google maps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,127 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const { compose, withProps, lifecycle } = require("recompose");
+const { withScriptjs, withGoogleMap, GoogleMap, Marker } = require("react-google-maps");
+const _ = require("lodash");
+const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox");
+
+const apiKey = "AIzaSyADdCfBug07EnHeVDoRmQExesiwKbgCOC4";
+
+
+const MyMapComponent = compose(
+    withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `610px` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+    }),
+    lifecycle({
+        componentWillMount() {
+            const refs = {}
+
+            this.setState({
+                bounds: null,
+                center: {
+                    lat: 41.9, lng: -87.624
+                },
+                markers: [],
+                onMapMounted: ref => {
+                    refs.map = ref;
+                },
+                onBoundsChanged: () => {
+                    this.setState({
+                        bounds: refs.map.getBounds(),
+                        center: refs.map.getCenter(),
+                    })
+                },
+                onSearchBoxMounted: ref => {
+                    refs.searchBox = ref;
+                },
+                onPlacesChanged: () => {
+                    const places = refs.searchBox.getPlaces();
+                    //const bounds = new google.maps.LatLngBounds();
+
+                    places.forEach(place => {
+                        if (place.geometry.viewport) {
+                            //bounds.union(place.geometry.viewport)
+                        } else {
+                            //bounds.extend(place.geometry.location)
+                        }
+                    });
+                    const nextMarkers = places.map(place => ({
+                        position: place.geometry.location,
+                    }));
+                    const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
+
+                    this.setState({
+                        center: nextCenter,
+                        markers: nextMarkers,
+                    });
+                    // refs.map.fitBounds(bounds);
+                },
+            })
+        },
+    }),
+    withScriptjs,
+    withGoogleMap)      ((props) =>
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    >
+        <SearchBox
+            ref={props.onSearchBoxMounted}
+            bounds={props.bounds}
+            controlPosition={2}
+            onPlacesChanged={props.onPlacesChanged}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <input
+                type="text"
+                placeholder="Search for a location"
+                style={{
+                    boxSizing: `border-box`,
+                    border: `1px solid transparent`,
+                    width: `240px`,
+                    height: `32px`,
+                    marginTop: `27px`,
+                    padding: `0 12px`,
+                    borderRadius: `3px`,
+                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                    fontSize: `14px`,
+                    outline: `none`,
+                    textOverflow: `ellipses`,
+                }}
+            />
+        </SearchBox>
+        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
+    </GoogleMap>
+    );
+
+class App extends Component{
+
+    constructor(props) {
+        super(props);
+        this.handleMarkerClick = this.handleMarkerClick.bind(this);
+
+        this.state = {
+            isMarkerShown: true
+        };
+    }
+
+    handleMarkerClick() {
+    };
+
+
+    render() {
+
+        return (
+            <div className="App">
+                <MyMapComponent
+                    isMarkerShown={this.state.isMarkerShown}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
