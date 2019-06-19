4aa276da2a1f32983d19863d89086168efce56ee
djzenma <eidma@aucegypt.edu>
2019-06-19 23:04:47 +0200

    New google maps Component

--- src/App.js
+++ src/App.js
@@ -1,101 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
-
-const { compose, withProps, lifecycle } = require("recompose");
-const { withScriptjs, withGoogleMap, GoogleMap, Marker } = require("react-google-maps");
-const _ = require("lodash");
-const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox");
+import {MapContainer} from "./Components/Map";
+import {GoogleApiWrapper} from "google-maps-react";
 
 const apiKey = "AIzaSyADdCfBug07EnHeVDoRmQExesiwKbgCOC4";
-
-
-const MyMapComponent = compose(
-    withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&v=3.exp&libraries=geometry,drawing,places",
-        loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `610px` }} />,
-        mapElement: <div style={{ height: `100%` }} />,
-    }),
-    lifecycle({
-        componentWillMount() {
-            const refs = {}
-
-            this.setState({
-                bounds: null,
-                center: {
-                    lat: 41.9, lng: -87.624
-                },
-                markers: [],
-                onMapMounted: ref => {
-                    refs.map = ref;
-                },
-                onBoundsChanged: () => {
-                    this.setState({
-                        bounds: refs.map.getBounds(),
-                        center: refs.map.getCenter(),
-                    })
-                },
-                onSearchBoxMounted: ref => {
-                    refs.searchBox = ref;
-                },
-                onPlacesChanged: () => {
-                    const places = refs.searchBox.getPlaces();
-                    //const bounds = new google.maps.LatLngBounds();
-
-                    places.forEach(place => {
-                        if (place.geometry.viewport) {
-                            //bounds.union(place.geometry.viewport)
-                        } else {
-                            //bounds.extend(place.geometry.location)
-                        }
-                    });
-                    const nextMarkers = places.map(place => ({
-                        position: place.geometry.location,
-                    }));
-                    const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
-
-                    this.setState({
-                        center: nextCenter,
-                        markers: nextMarkers,
-                    });
-                    // refs.map.fitBounds(bounds);
-                },
-            })
-        },
-    }),
-    withScriptjs,
-    withGoogleMap)      ((props) =>
-    <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-    >
-        <SearchBox
-            ref={props.onSearchBoxMounted}
-            bounds={props.bounds}
-            controlPosition={2}
-            onPlacesChanged={props.onPlacesChanged}
-        >
-            <input
-                type="text"
-                placeholder="Search for a location"
-                style={{
-                    boxSizing: `border-box`,
-                    border: `1px solid transparent`,
-                    width: `240px`,
-                    height: `32px`,
-                    marginTop: `27px`,
-                    padding: `0 12px`,
-                    borderRadius: `3px`,
-                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                    fontSize: `14px`,
-                    outline: `none`,
-                    textOverflow: `ellipses`,
-                }}
-            />
-        </SearchBox>
-        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
-    </GoogleMap>
-    );
 
 class App extends Component{
 
@@ -104,7 +12,6 @@ class App extends Component{
         this.handleMarkerClick = this.handleMarkerClick.bind(this);
 
         this.state = {
-            isMarkerShown: true
         };
     }
 
@@ -116,12 +23,12 @@ class App extends Component{
 
         return (
             <div className="App">
-                <MyMapComponent
-                    isMarkerShown={this.state.isMarkerShown}
-                />
+                <MapContainer google={window.google}/>
             </div>
         );
     }
 }
 
-export default App;
+export default GoogleApiWrapper ({
+    apiKey: apiKey
+})(MapContainer);
---
+++ src/Components/Map.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+const mapStyles = {
+    width: '100%',
+    height: '100%',
+};
+
+export class MapContainer extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedPlace: {
+                name: 'hehe'
+            },
+            stores: [{lat: 47.49855629475769, lng: -122.14184416996333},
+                {latitude: 47.359423, longitude: -122.021071},
+                {latitude: 47.2052192687988, longitude: -121.988426208496},
+                {latitude: 47.6307081, longitude: -122.1434325},
+                {latitude: 47.3084488, longitude: -122.2140121},
+                {latitude: 47.5524695, longitude: -122.0425407}]
+        };
+    }
+
+    displayMarkers = () => {
+        return this.state.stores.map((store, index) => {
+            return <Marker key={index} id={index} position={{
+                lat: store.latitude,
+                lng: store.longitude
+            }}
+                           onClick={() => console.log("You clicked me!")} />
+        });
+    }
+
+    onMarkerClick() {
+
+    }
+
+    onInfoWindowClose() {
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                <Map google={this.props.google}
+                     zoom={14}
+                     style={mapStyles}
+                     initialCenter={{ lat: 47.444, lng: -122.176}}
+                >
+                    <Marker onClick={this.onMarkerClick}
+                            name={'Current location'} />
+
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                        <div>
+                            <h1>{this.state.selectedPlace.name}</h1>
+                        </div>
+                    </InfoWindow>
+
+                    {this.displayMarkers()}
+                </Map>
+            </div>
+        );
+    }
+}
